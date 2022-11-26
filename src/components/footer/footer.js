f80960e20f6d0001460763ec246d0fe03ab41d66
Sebastián Fernando Cortés Cáceres <scortesc@MacBook-Air-de-Sebastian.local>
2022-11-26 20:31:04 -0300

    se actualiza footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,30 +4,31 @@ export default function Footer() {
     return (
 
         <footer>
+            <div className="footer-content">
 
-            <div className="container__footer">
+                <h3>Pizzeria D&S</h3>
+                <p>Pizzeria D&S tienen años de experiencia en la elaboración de las mejores masas tradicionales y hechas en casa
+                    y con los ingredientes mas naturales y de alta calidad.</p>
 
-                <div className="box__footer">
-                    <i>Compañia</i>
-                    <i>Acerca de</i>
-                    <i>Quieres trabajar con nosotros</i>
-                    <i>Contacto</i>
-                    <i>Formulario de contacto</i>
-                </div>
-
-                <div className="box__footer">
-
-                    <h1>Redes sociales</h1>
-                    <h2>Whatsaap <ion-icon name="logo-whatsapp"></ion-icon></h2>
-                    <h2>Facebook <ion-icon name="logo-facebook"></ion-icon></h2>
-                    <h2>Twitter <ion-icon name="logo-twitter"></ion-icon></h2>
-                    <h2>Instagram <ion-icon name="logo-instagram"></ion-icon></h2>
-                </div>
-
+                <ul className="socials">
+                <i><ion-icon name="logo-facebook"></ion-icon></i>
+                <i><ion-icon name="logo-twitter"></ion-icon></i>
+                <i><ion-icon name="logo-instagram"></ion-icon></i>
+                <i><ion-icon name="logo-linkedin"></ion-icon></i>
+                </ul>
             </div>
 
-            <div className="box__copyright">
-                <p> Todos los derechos reservadors por los estudiantes del Bootcamp Desarrollador Full Stack UDD <b>D&S</b></p>
+            <div className="footer-bottom">
+            <p> Todos los derechos reservadors por los estudiantes del Bootcamp Desarrollador Full Stack UDD <b>D&S</b></p>
+            </div>
+
+            <div className="footer-menu">
+                <ul className="f-menu">
+                <li><i href="">Inicio</i></li>
+                <li><i href="">Sobre nosotros</i></li>
+                <li><i href="">Contactos</i></li>
+                <li><i href=""></i>Quieres trabajar con nosotros</li>
+                </ul>
             </div>
         </footer>
 
