4bf5d1f41e930d7dd5fcf92cfc52ca7b333b5553
Sebastián Fernando Cortés Cáceres <scortesc@MacBook-Air-de-Sebastian.local>
2022-11-26 15:22:54 -0300

    se actualiza menu js y css

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -2,27 +2,43 @@ import "./menu.css"
 
 export default function Menu() {
   return (
-  <div>
-  
-    <div>
-      <h1> Menú section</h1>
-    </div>
-    <div >
-      <div className="menu">
-        <img className="izquierda" width={150} src= "https://thumbs.dreamstime.com/b/pizza-35669930.jpg" alt=""></img>
-        Este párrafo tiene insertada una imagen al principio del párrafo. Se trata del logotipo de GNU. GNU es un acrónimo recursivo que significa "GNU is Not Unix" (GNU No es Unix). 
-        
-        <p> <img className="img1"src= "https://thumbs.dreamstime.com/b/pizza-35669930.jpg" alt=""></img></p>
+
+<div>
+
+<h1>Nuestro Menu</h1>
+
+
+<section className="tarjetas">
+
+
+      <div className="menu1">
+        <img src="https://thumbs.dreamstime.com/b/pizza-17898986.jpg" alt="" />
+        <p className="text1">
+          <h1>Pizza Napolitana</h1>
+          Exquisita pizza de mozzarella, jamón y tomate al oregano para los amantes del sabor napolitano.
+          <h1>8.990</h1>
+        </p>
       </div>
-      
-      
-    </div>    
-    
-  </div>
-  
-  
-  
-  
-  
+
+      <div className="menu2">
+        <img src="https://thumbs.dreamstime.com/b/pizza-35669930.jpg" alt="" />
+        <p className="text2">
+          <h1>Pizza Margarita</h1>
+          Exquisita pizza de mozzarella, aceitunas, tomates frescos y albahaca.
+          <h1>8.990</h1>
+        </p>
+      </div>
+
+      <div className="menu3">
+        <img src="https://thumbs.dreamstime.com/b/pizza-menu-peperoni-pizza-menu-food-cover-background-247625216.jpg" alt=""/>
+        <p className="text3">
+          <h1>Pizza Pepperoni</h1>
+          Exquisita pizza de mozzarella y doble pepperoni para los Pepperoni lovers.
+          <h1>8.990</h1>
+        </p>
+      </div>
+
+      </section>
+      </div>
   )
 }
